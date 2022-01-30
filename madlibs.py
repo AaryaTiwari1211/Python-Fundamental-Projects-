0ce6864e3933906dcaff266c08b66f5dffffbb9a
Aarya <aarya.tiwari@somaiya.edu>
2022-01-30 13:20:28 +0530

    madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,4 @@
-    
+color=input("Enter any colour: ")
+flower=input("Enter any flower: ")
+char_name=input("Enter the person you love: ")
+print("It was a wonderful day. The "+color+" sky was as bright as ever. those "+flower+ " where shining and swaying in the cozy winds.I always wanted to go to this place with " +char_name+ " To have a wonderful time altogether")    
