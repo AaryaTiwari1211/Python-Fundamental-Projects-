293295369bc9cbca0cd585cc605df60027fbeeaf
Aarya <aarya.tiwari@somaiya.edu>
2022-01-30 18:02:03 +0530

    lists.py

---
+++ lists.py
@@ -0,0 +1,20 @@
+friends = ["Atharva","Eshan","Pratham","Dhairya","Aditya","Shrivatsa","Dev"]
+enemies = ["Mayank", "BTS" , "Sahil","Kinda"]
+friends[2]="Pratherium"
+friends.append("Rahul")
+print(friends)
+friends.insert(3,"Jay")
+print(friends)
+friends.remove("Atharva")
+print(friends)
+friends.pop()
+print(friends)
+print(friends.index("Dhairya"))
+print(friends.count("Eshan"))
+friends.sort()
+print(friends)
+print(friends[-2])
+print(friends[1:4])
+print(friends[2:])
+
+
